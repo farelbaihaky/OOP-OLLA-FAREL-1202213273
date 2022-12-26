00fed8f991ac0e3eb46f38bcd28d885641f541b7
farelbaihaky <116346629+farelbaihaky@users.noreply.github.com>
2022-12-26 08:41:44 +0700

    Add files via upload

---
+++ OOP-OLLA-FAREL-1202213273/MODUL3_FAREL/src/Calculation.java
@@ -0,0 +1,20 @@
+public class Calculation implements Runnable{
+    private final double radius;
+    private final double side;
+    private final double area;
+    static double phi = 3.14;
+    @Override
+    public void run(){
+        while (true) {
+            
+        }
+    }
+    public Calculation(double radius, double side, double area){
+        this.radius = radius;
+        this.side = side;
+        this.area = area;
+    }
+public void setSquare()
+
+
+}
---
+++ OOP-OLLA-FAREL-1202213273/MODUL3_FAREL/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Calculation z = new Calculation();
+    double radius, side, area;
+    try{
+        System.out.println("==Menu Progra,==");
+        System.out.println("1. Square");
+        System.out.println("2. Circle");
+        System.out.println("3. Trapezoid");
+        System.out.println("Select Menu : ");
+        menu = input.nextdouble();
+        System.out.println("\n enter the radius of the circle : ");
+        radius = input.nextdouble()
+    }
+    }
+}
